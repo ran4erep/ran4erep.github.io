0ee3ea54e6ee5ed21aca5d453ca5f292a246af80
ran4erep <45404569+ran4erep@users.noreply.github.com>
2020-09-01 16:56:11 +0300

    iswork?

--- cod/init.js
+++ cod/init.js
@@ -105,6 +105,7 @@ let konamiInput = "";
 let LOSFOV = 5;
 let r = 50, g = 50, b = 50;
 let tilesetProperties;
+let tilesetJSONLoaded = false;
 
 let loadJSON = (file) => {
 	let requestURL = file;
@@ -114,6 +115,26 @@ let loadJSON = (file) => {
 	request.send();
 	request.onload = function() {
 		tilesetProperties = request.response;
+		for(let i=0; i < maps[currentMap].length; i++) {
+	for(let j=0; j < maps[currentMap].length; j++) {
+		if(tilesetProperties.tiles[maps[currentMap][j][i]].objectgroup) {
+			collisionMap[j][i].x = (i*spriteSize) + tilesetProperties.tiles[maps[currentMap][j][i]].objectgroup.objects[0].x;
+			collisionMap[j][i].y = (j*spriteSize) + tilesetProperties.tiles[maps[currentMap][j][i]].objectgroup.objects[0].y;
+			collisionMap[j][i].width = tilesetProperties.tiles[maps[currentMap][j][i]].objectgroup.objects[0].width;
+			collisionMap[j][i].height = tilesetProperties.tiles[maps[currentMap][j][i]].objectgroup.objects[0].height;
+		} else {
+			collisionMap[j][i].x = 0;
+			collisionMap[j][i].y = 0;
+			collisionMap[j][i].width = 0;
+			collisionMap[j][i].height = 0;
+		}
+		bakedCollision++;
+		//ctx.fillStyle = "red";
+	//drawBar(percentOf(bakedCollision,maps[currentMap].length*maps[currentMap].length),(canvas.width/2)-50,(canvas.height/2)-10,100,10);
+	}
+}
+
+
 	}
 }
 
